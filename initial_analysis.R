f6748e4376d2f1e090ff14366b9fdb18ea6e8e64
Catalina Bush <157786091+catabush05@users.noreply.github.com>
2026-03-03 17:11:40 -0800

    Exploratory analysis

---
+++ initial_analysis.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+data <- read_csv("hf182-01-trees-current-env.csv")
+data$prop <- data$fagus / data$count
+
+
+## Density of proportion metric
+
+ggplot(data = data, aes(x = prop)) +
+  geom_density(color="blue", fill = "blue", alpha = 0.5) +
+  labs(
+    y = "Density", 
+    x = "Beech presence proportion"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(family='', face='bold', size=20),
+    plot.subtitle = element_text(family='', color="#757575", size=14)
+  )
+
+
+## Beech presence vs Annual temp
+
+ggplot(data = data, aes(x = bio1.0, y = prop)) +
+  geom_point(aes(x = bio1.0, y = prop), color="blue", alpha = 0.5) +
+  labs(
+    y = "Beech Presence", 
+    x = "Mean Annual Temperature (z-score)", 
+    title = "Higher annual temps correlate with less Beech trees", 
+    subtitle = "Gridded data from 2010-2011 across U.S. & Canada"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(family='', face='bold', size=20),
+    plot.subtitle = element_text(family='', color="#757575", size=14)
+  )
